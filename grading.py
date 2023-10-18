618fba918873bf58af17d036e7f800b4cfc9febf
Truphy <truphenaokenye@gmail.com>
2023-10-18 12:02:19 +0300

    added few commands

--- grading.py
+++ grading.py
@@ -1,23 +1,27 @@
-
-math=float(input("enter your math marks"))
-phy=float(input("enter your english marks"))
-geo=float(input("enter your geogrphy marks"))
-chem=float(input("enter your chemistry marks"))
-
-
-Total = math + phy + geo + chem
-Avg = Total/4
-
-if Avg < 0:
-    print("Unrecognized average (Negative)")
-
-elif Avg > 100:
-    print("Unrecognized average (Greater than 100)")
-elif Avg >= 71:
-        print("A")
-elif Avg >= 51:
-        print("B")
-elif Avg >= 31:
-        print("C")
+math = float(input("Enter your math marks:"))
+if math < 0 or math > 100:
+    print("Unrecognized value (Negative or Greater than 100)")
 else:
-        print("D")
+    phy = float(input("Enter your english marks:"))
+    if phy < 0 or phy > 100:
+        print("Unrecognized value (Negative or Greater than 100)")
+    else:
+        geo = float(input("Enter your geography marks:"))
+        if geo < 0 or geo > 100:
+            print("Unrecognized value (Negative or Greater than 100)")
+        else:
+            chem = float(input("Enter your chemistry marks:"))
+            if chem < 0 or chem > 100:
+                print("Unrecognized value (Negative or Greater than 100)")
+            else:
+                Total = math + phy + geo + chem
+                Avg = Total / 4
+
+                if Avg >= 71:
+                    print("A")
+                elif Avg >= 51:
+                    print("B")
+                elif Avg >= 31:
+                    print("C")
+                else:
+                    print("D")
