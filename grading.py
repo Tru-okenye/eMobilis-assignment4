850370e2d711c047c97b2ed80d14bc4a5ea47aa0
Truphy <truphenaokenye@gmail.com>
2023-10-18 12:36:25 +0300

    added a function

--- grading.py
+++ grading.py
@@ -1,27 +1,24 @@
-math = float(input("Enter your math marks:"))
-if math < 0 or math > 100:
-    print("Unrecognized value (Negative or Greater than 100)")
-else:
-    phy = float(input("Enter your english marks:"))
-    if phy < 0 or phy > 100:
-        print("Unrecognized value (Negative or Greater than 100)")
-    else:
-        geo = float(input("Enter your geography marks:"))
-        if geo < 0 or geo > 100:
-            print("Unrecognized value (Negative or Greater than 100)")
+def get_valid_input(prompt):
+    while True:
+        value = float(input(prompt))
+        if 0 <= value <= 100:
+            return value
         else:
-            chem = float(input("Enter your chemistry marks:"))
-            if chem < 0 or chem > 100:
-                print("Unrecognized value (Negative or Greater than 100)")
-            else:
-                Total = math + phy + geo + chem
-                Avg = Total / 4
+            print("Invalid input. Please enter a value between 0 and 100.")
 
-                if Avg >= 71:
-                    print("A")
-                elif Avg >= 51:
-                    print("B")
-                elif Avg >= 31:
-                    print("C")
-                else:
-                    print("D")
+math = get_valid_input("Enter your math marks: ")
+phy = get_valid_input("Enter your english marks: ")
+geo = get_valid_input("Enter your geography marks: ")
+chem = get_valid_input("Enter your chemistry marks: ")
+
+Total = math + phy + geo + chem
+Avg = Total / 4
+
+if Avg >= 71:
+    print("A")
+elif Avg >= 51:
+    print("B")
+elif Avg >= 31:
+    print("C")
+else:
+    print("D")
